3143d70c85d5e17e5110400f738f24ff7f38fa6f
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2021-11-30 12:14:28 +0100

    Datenteypen

---
+++ 01 Varibalen/Datentypen.ps1
@@ -0,0 +1,10 @@
+cls
+$ausgabetext = "Hallo Welt"
+$ausgabetext.GetType().Name
+write-host $ausgabetext
+
+write-host "------------------"
+
+$ausgabetext=3.2
+$ausgabetext.GetType().Name
+write-host $ausgabetext
