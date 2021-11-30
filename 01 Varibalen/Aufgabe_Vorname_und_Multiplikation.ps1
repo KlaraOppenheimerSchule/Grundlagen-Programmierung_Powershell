06880036569719bb88e9c93d471f531a68f0e8a6
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2021-11-30 12:33:23 +0100

    Aufgabe 1,2

---
+++ 01 Varibalen/Aufgabe_Vorname_und_Multiplikation.ps1
@@ -0,0 +1,11 @@
+#Aufgabe 1
+$vorname=Read-Host
+$nachname=Read-Host
+write-host $vorname $nachname 
+
+#Aufgabe 2
+[int]$nutzer1=Read-Host
+[int]$nutzer2=Read-Host
+[int]$nutzer3=Read-Host
+[int]$ergebnis = ($nutzer1 * $nutzer2 * $nutzer3)
+write-host $ergebnis
