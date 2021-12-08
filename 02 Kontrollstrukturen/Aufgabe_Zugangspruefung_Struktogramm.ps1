1534c44ab3d1d206f785aa86504b8066a2b34576
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2021-12-08 12:51:01 +0100

    Zahlenraten erweitert

---
+++ 02 Kontrollstrukturen/Aufgabe_Zugangspruefung_Struktogramm.ps1
@@ -0,0 +1,38 @@
+$random = Get-Random -maximum 10 -minimum 1
+write-host "Zufallszahl: $random"
+
+write-host Geben Sie eine Zahl zwischen 1 und 10 ein
+[int]$zahluser1 = read-host
+
+if($random -eq $zahluser1)
+{
+    write-host Sie haben richtig geschätzt, die Zahl ist $random
+}
+else
+{
+    if($random -gt $zahluser1)
+    {
+        write-host Die zufällige Zahl ist größer als die geschätzte Zahl
+    }
+    else
+    {
+        write-host Die zufällige Zahl ist kleiner als die geschätzte Zahl
+    }
+
+    [int]$zahluser1 = read-host
+    if($random -eq $zahluser1)
+    {
+        write-host Sie haben richtig geschätzt, die Zahl ist $random
+    }
+    else
+    {
+        if($random -gt $zahluser1)
+        {
+            write-host Die zufällige Zahl ist größer als die geschätzte Zahl
+        }
+        else
+        {
+            write-host Die zufällige Zahl ist kleiner als die geschätzte Zahl
+        }        
+    }
+}
