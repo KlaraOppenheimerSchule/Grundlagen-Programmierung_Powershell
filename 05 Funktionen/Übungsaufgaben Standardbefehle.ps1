481c1f140b9b7ff20930a3b8805a94e17a828d00
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2022-05-16 14:51:55 +0200

    more functions

---
+++ 05 Funktionen/Übungsaufgaben Standardbefehle.ps1
@@ -0,0 +1,28 @@
+﻿#1
+Get-ChildItem
+
+#2
+$pi=[Math]::PI
+write-host $pi
+
+#3
+$date=get-date
+write-host $date
+
+#4
+Get-Random -min 0 -max 100
+
+#5
+$zahlen=1,4,8,12
+#Kurzversion
+$zahlen | measure-object -Average
+
+#Langversion
+$summe=0
+foreach($element in $zahlen)
+{
+    $summe=$summe + $element
+}
+
+write-host ($summe/$zahlen.Count)
+
