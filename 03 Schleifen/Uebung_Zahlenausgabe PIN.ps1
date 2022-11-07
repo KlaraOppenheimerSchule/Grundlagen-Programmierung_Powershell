caed9345976f5a144b1d2f679598787ade5deb0c
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2022-11-07 18:48:16 +0100

    Schleifenübung

--- 03 Schleifen/Uebung_Zahlenausgabe PIN.ps1
+++ 03 Schleifen/Uebung_Zahlenausgabe PIN.ps1
@@ -13,7 +13,6 @@ for($i=0; $i -le 9999; $i++)
 
 
 #Erweiterte Lösung mit führenden 0-Stellen
-
 for($stelleZaehler1=0; $stelleZaehler1 -lt 10; $stelleZaehler1++)
 {
     for($stelleZaehler2=0; $stelleZaehler2 -lt 10; $stelleZaehler2++)
@@ -22,12 +21,7 @@ for($stelleZaehler1=0; $stelleZaehler1 -lt 10; $stelleZaehler1++)
         {
             for($stelleZaehler4=0; $stelleZaehler4 -lt 10; $stelleZaehler4++)
             {
-                write-host "$stelleZaehler1$stelleZaehler2$stelleZaehler3$stelleZaehler4"
-                [int]$wert=$stelleZaehler1+$stelleZaehler2+$stelleZaehler3+$stelleZaehler4
-                if($wert -eq 1000)
-                {
-                    $break=read-host
-                }
+                write-host "$stelleZaehler1 $stelleZaehler2 $stelleZaehler3 $stelleZaehler4"
             }
         }
     }
