c7afcdefd2a87be54d56b9608136653b0916e56d
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2022-12-14 15:43:57 +0100

    Übung Partner

--- 03 Schleifen/Uebung_Partnerinnen.ps1
+++ 03 Schleifen/Uebung_Partnerinnen.ps1
@@ -1,3 +1,4 @@
+#Aufgabe b
 $summeAlter=0
 
 for($i=1; $i -lt 5; $i++)
@@ -9,6 +10,7 @@ for($i=1; $i -lt 5; $i++)
 $durchschnittsAlter=$summeAlter/4
 write-host "Das Durchschnittsalter beträgt: $durchschnittsAlter"
 
+
 #Alternativ mit einer While-Schleife
 $summeAlter=0
 $zaehler=1
@@ -19,4 +21,66 @@ while($zaehler -lt 5)
     $zaehler=$zaehler+1
 }
 $durchschnittsAlter=$summeAlter/4
+write-host "Das Durchschnittsalter beträgt: $durchschnittsAlter"
+
+
+#Aufgabe c) Erweiterung um Beuteschema von Christoph
+$summeAlter=0
+$zaehler=1
+while($zaehler -lt 5)
+{
+    [int]$alter=read-host "Bitte gebe dein Alter ein"
+    if($alter -gt 19 -and $alter -lt 31)
+    {
+        $summeAlter=$summeAlter+$alter
+        $zaehler=$zaehler+1  
+    }
+    else 
+    {
+        write-host "Bist leider außerhalb meines Beuteschemas"
+    }
+}
+$durchschnittsAlter=$summeAlter/4
+write-host "Das Durchschnittsalter beträgt: $durchschnittsAlter"
+#>
+
+#Aufgabe c) mit for-Schleife
+$summeAlter=0
+[int]$anzahlParterinnen=read-host "Wie viele Bekanntschaften möchtest du heute haben"
+for($i=1; $i -lt $anzahlParterinnen; $i++)
+{
+    [int]$alter=read-host "Bitte gebe dein Alter ein"
+    if($alter -gt 19 -and $alter -lt 31)
+    {
+        $summeAlter=$summeAlter+$alter
+    }
+    else 
+    {
+        write-host "Bist leider außerhalb meines Beuteschemas"
+        $i=$i-1
+    }
+}
+
+$durchschnittsAlter=$summeAlter/$anzahlParterinnen
+write-host "Das Durchschnittsalter beträgt: $durchschnittsAlter"
+
+
+#Aufgabe d) mit for-Schleife
+$summeAlter=0
+[int]$anzahlParterinnen=read-host "Wie viele Bekanntschaften möchtest du heute haben"
+for($i=1; $i -le $anzahlParterinnen; $i++)
+{
+    [int]$alter=read-host "Bitte gebe dein Alter ein"
+    if($alter -gt 19 -and $alter -lt 31)
+    {
+        $summeAlter=$summeAlter+$alter
+    }
+    else 
+    {
+        write-host "Bist leider außerhalb meines Beuteschemas"
+        $i=$i-1
+    }
+}
+
+$durchschnittsAlter=$summeAlter/$anzahlParterinnen
 write-host "Das Durchschnittsalter beträgt: $durchschnittsAlter"
