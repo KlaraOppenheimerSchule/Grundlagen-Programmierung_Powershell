b7c1554393d8696b968aff8456752f1f7d80d5cf
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2023-01-30 12:02:00 +0100

    Exercise

--- 03 Schleifen/Einfuehrung_Schleifen.ps1
+++ 03 Schleifen/Einfuehrung_Schleifen.ps1
@@ -1,10 +1,10 @@
+#Kopfgesteuerte Schleifen
 #Zähle eine Zahl von 0 bis 10 hoch
 <#
 for($i=0; $i -lt 11; $i++)
 {
     write-host $i
 }
-#>
 
 #Alternativ while-Schleife
 $y=0
@@ -16,8 +16,22 @@ while($y -lt 11)
 #Foreach
 
 
-
+#Fußgesteuerte Schleifen
 #Do-while
+$i=0
+do
+{
+    write-host $i
+    $i++
+}while($i -lt 11)
+#Schleife wird solange ausgeführt/wiederholt, SOLANGE die Bedingung zutrifft
+#>
 
 #Do-until
-
+$i=0
+do
+{
+    write-host $i
+    $i++
+}until($i -gt 10)
+#Schleife wird solange ausgeführt, BIS die Bedingung zutrifft
