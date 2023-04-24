435f48f8a66420cf65a820d86b3518ed77c805b9
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2023-04-24 12:02:25 +0200

    Übung Arrayfreunde

--- 04 Komplexe Datentypen/EinfuehrungArrayList.ps1
+++ 04 Komplexe Datentypen/EinfuehrungArrayList.ps1
@@ -1,14 +1,33 @@
-$essensWuensche = New-Object System.Collections.ArrayList
+#2a
+$freundesListe = New-Object System.Collections.ArrayList
 
-[void]$essensWuensche.Add("Klaus")
-[void]$essensWuensche.Add("Klaus1")
-[void]$essensWuensche.Add("Klaus2")
-[void]$essensWuensche.Add("Klaus3")
-[void]$essensWuensche.Add("Klaus4")
+[void]$freundesListe.Add("Klaus")
+[void]$freundesListe.Add("Fred")
+[void]$freundesListe.Add("Helmut")
 
-$essensWuensche.Contains("Klaus")
-$essensWuensche.IndexOf("Klaus3")
-$essensWuensche.Remove("Klaus4")
+write-host $freundesListe
 
-$essensWuensche[2]="Uschi"
-write-host $essensWuensche
+#2b
+$freundesListe.RemoveAt(2)
+write-host $freundesListe
+
+#2c
+$freundesListe.add("Uschi")
+write-host $freundesListe
+
+#2d
+$freundesListe.add((read-host "Neuen Freund"))
+
+
+
+
+
+
+
+
+#$essensWuensche.Contains("Klaus")
+#$essensWuensche.IndexOf("Klaus3")
+#$essensWuensche.Remove("Klaus4")
+
+#$essensWuensche[2]="Uschi"
+#write-host $essensWuensche
---
+++ 04 Komplexe Datentypen/Uebung_Reihenfolgetauschen.ps1
@@ -0,0 +1,27 @@
+#A1
+$rechnerListe=  New-Object System.Collections.ArrayList
+$rechnerListe.add("YXY...")
+
+
+
+
+$zahlenReihen=  New-Object System.Collections.ArrayList
+
+[void]$zahlenReihen.Add(0)
+[void]$zahlenReihen.Add(1)
+[void]$zahlenReihen.Add(2)
+[void]$zahlenReihen.Add(3)
+[void]$zahlenReihen.Add(4)
+[void]$zahlenReihen.Add(5)
+[void]$zahlenReihen.Add(6)
+[void]$zahlenReihen.Add(7)
+[void]$zahlenReihen.Add(8)
+[void]$zahlenReihen.Add(9)
+
+
+write-host "Zahlenreihe zuvor:"
+$zahlenReihen
+
+$zahlenReihen.Reverse()
+write-host "Zahlenreihe danach:"
+$zahlenReihen
