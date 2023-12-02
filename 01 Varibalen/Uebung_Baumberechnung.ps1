ce87b35d3a926fb3afcf976db9a013c31df3293a
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2023-12-02 16:55:18 +0100

    Some comments

--- 01 Varibalen/Uebung_Baumberechnung.ps1
+++ 01 Varibalen/Uebung_Baumberechnung.ps1
@@ -1,5 +1,7 @@
+#PI wird von der .NET-Klasse Math bezogen
 [float]$pi = [math]::PI
 [float]$L = Read-host  "L in Metern: "
 [float]$D = Read-Host  "Durchmesser in cm: "
+
 [float]$result = (($pi/4*$D*$D)*$L/10000)
 write-host($result)
